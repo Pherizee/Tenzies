572e476cebaeaa0ad38c0a7e83dab949e76bc82c
Jesuferanmi Asiyanbi <asiyanbijesuferanmi@gmail.com>
2022-03-23 12:18:29 +0100

    Add animated number faces and dot faces

--- src/NumDie.jsx
+++ src/NumDie.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react';
+
 const NumDie = ({ num, isHeld, holdDie }) => {
+  const [presentClass, setPresentClass] = useState("");
+  
+  useEffect(() => {
+    setPresentClass(`show__${num}`)
+  }, [num])
+
   const styles = {
     backgroundColor: isHeld ? "#59e391" : "#fff",
   }
 
+  function generateFullDie() {
+    const fullDie = []
+    for(let i = 1; i < 7; i++) {
+      fullDie.push(<div className={`die__face die__face__${i} die__face-num`} style={styles}>{i}</div>);
+    }
+    return fullDie;
+  }
+
+  const fullDie = [
+    <div className={`anim__die ${presentClass}`}>
+      {generateFullDie()}
+    </div>
+  ]
+
   return (
-    <div className={`die`} style={styles} onClick={holdDie}>
-      {num}
+    <div className="die__container" onClick={holdDie}>
+      {fullDie}
     </div>
   );
 };
